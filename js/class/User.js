ca56bd8bc46d77ba473dedd344a90f70f2b8f899
Zhiying Huang <ZhiyingHuang2023@outlook.com>
2024-04-09 21:22:10 +0300

    sidebar完成&navbar颜色样式修改&logout功能完成

--- js/class/User.js
+++ js/class/User.js
@@ -18,23 +18,27 @@ class User {
   }
 
   get id() {
-    return this.#id;
+    return this.#id
   }
 
   get username() {
-    return this.#username;
+    return this.#username
   }
 
   get email() {
-    return this.#email;
+    return this.#email
   }
 
   get password() {
-    return this.#password;
+    return this.#password
   }
 
   get newPassword() {
-    return this.#newPassword;
+    return this.#newPassword
+  }
+
+  get isLoggedIn(){
+    return this.#username !== undefined ? true : false
   }
 
   async login(username, password) {
@@ -196,5 +200,13 @@ class User {
       throw error;
     }
   }
+
+  logout() {
+    this.#username = undefined
+    sessionStorage.removeItem('user')
+  }
 }
+
+
+
 export { User };
