8fe7817aab08fbe2abff2045fc7af4e9ac2cb8ba
Taisheng Chen <ists.chen@gmail.com>
2024-04-09 14:23:42 +0300

    add the backend code for create the new post and create a new code file in server folder called post.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { userRouter } = require('./routes/user.js')
+const { postRouter } = require('./routes/post.js')
 
 const port = 3001
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/user',userRouter) 
+app.use('/post',postRouter)
 
 app.listen(port,() => {
   console.log(`Server is listening on port ${port}`)
