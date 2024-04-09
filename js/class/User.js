a915113b9d07546d24c98d9412dde796721a0a03
Taisheng Chen <ists.chen@gmail.com>
2024-04-09 19:47:17 +0300

    修复了newpost前后端链接不上的问题

--- js/class/User.js
+++ js/class/User.js
@@ -178,6 +178,10 @@ class User {
       username: username,
     });
 
+    // console.log(data.title)
+    // console.log(data.content)
+    // console.log(data.username)
+
     try {
       const response = await fetch(BACKEND_URL + "/post/create", {
         method: "POST",
