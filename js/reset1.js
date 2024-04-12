87852dc9fbfda3bc6e816f42286514eff9cdd8fd
Linna <haolinna23@gmail.com>
2024-04-12 23:49:28 +0300

    修正reset1邮箱不存在时的报错问题

--- js/reset1.js
+++ js/reset1.js
@@ -32,10 +32,15 @@ btn.addEventListener("click", async (event) => {
   try {
     const exists = await user.checkEmailExists(email);
 
+    // text
+    // const exists = false;
+    // console.log(exists);
+
     if (exists) {
       // if exist, to reset2.html
       errorMessage.style.display = "none";
       window.location.href = "../reset2.html";
+<<<<<<< HEAD
     }
     // else {
     //   // if not exist, display error info
@@ -45,13 +50,19 @@ btn.addEventListener("click", async (event) => {
   } catch (error) {
     let errorMessage = document.querySelector("#error");
     if (error.message === "not found") {
+=======
+      // store email to local
+      localStorage.setItem("email", email);
+    } else {
+>>>>>>> 2915d3e (修正reset1邮箱不存在时的报错问题)
       // if not exist, display error info
+      let errorMessage = document.querySelector("#error");
       errorMessage.style.display = "block";
       errorMessage.textContent = "Email does not exist.";
-    } else {
-      console.error("Error:", error);
     }
+  } catch (error) {
+    console.error("Error:", error);
+    errorMessage.style.display = "block";
+    errorMessage.textContent = "An error occurred. Please try again later.";
   }
 });
-
-// 222
